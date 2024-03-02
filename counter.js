d4097a5498f5b9b27b1212bca645f20735e329c0
Fabiola Kammi <fabiolakammiladouce@gmail.com>
2024-03-02 10:53:28 -0500

    first commit

---
+++ counter.js
@@ -0,0 +1,24 @@
+let currentCount = "0"
+function updateDisplay (){
+
+}
+
+function increaseCounter(){
+    document.addEventListener(onclick)
+    document.getElementById(increaseButton)
+    currentCount++;
+    console.log(currentCount++)
+}
+function decreaseCounter(){
+    
+    document.addEventListener(onclick)
+    document.getElementById(decreaseButton)
+    currentCount--;
+}
+
+function resetCounter(){
+    document.addEventListener(onclick)
+    document.getAnimations(resetButton)
+    currentCount === 0;
+}
+
